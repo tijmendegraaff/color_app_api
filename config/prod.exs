ad186fc29e3e1a17619e955b6bda104c5dd347e1
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-16 18:17:16 +0200

    setup for heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,16 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :color_app_api, ColorAppApiWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  load_from_system_env: true,
+  url: [scheme: "https", host: "afternoon-mesa-94126.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :color_app_api, ColorAppApi.Repo,
+  pool_size: 18,
+  ssl: true,
+  url: System.get_env("DATABASE_URL")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,4 +70,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ColorAppApi.MixProject do
     [
       app: :color_app_api,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
