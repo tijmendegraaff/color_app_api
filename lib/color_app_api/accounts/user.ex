32e51f1c477cdf94ec463e658c94be3eef23cd55
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-18 13:07:30 +0200

    add user model

---
+++ lib/color_app_api/accounts/user.ex
@@ -0,0 +1,23 @@
+defmodule ColorAppApi.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:first_name, :last_name, :email, :password])
+    |> validate_required([:first_name, :last_name, :email, :password])
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
+  end
+end
